b203879f5536e7f22ab9cc33bfa4073fbe5774ae
Lê Thanh Vạn <odon29032005@gmail.com>
2026-02-18 15:28:25 +0700

    Add files via upload

---
+++ Chapter 1 Getting started with SQL.sql
@@ -0,0 +1,12 @@
+-- 1.1. Overview 
+
+-- SQL ( Structured Query Language ) is a speacial-purpose programming language designed for manageing data held 
+-- in a Relational Database Management System (RDBMS). SQl-like languages can also be used in Relational Data Stream Management Systems (RDSMS),
+-- or in 'not-only SQL"(NoSQL) databases.
+
+
+-- SQL comprises of 3 major sub-languages:
+-- 1. Data Definition Language (DDL): to create and modify the structure of the database
+-- 2. Data Manipulation Language (DML): to perform Read, Insert, Update and Delete operators on the data of the database;
+-- 3. Data Control Language (DCL): to control the access of the data stored in the database
+
